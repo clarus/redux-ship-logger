2b9999c36648dc7b6c1e70428e8e702e9542a164
Guillaume Claret <dev@clarus.me>
2016-10-15 12:39:05 +0200

    Add a log function for the commits

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 // @flow
+/* eslint-disable no-console */
 import type {Ship, Snapshot} from 'redux-ship';
 import {snap} from 'redux-ship';
 
@@ -26,13 +27,34 @@ function style(color: string): string {
   return `color: ${color}; font-weight: bold`;
 }
 
+export function logCommit<Commit, Patch, State>(
+  applyCommit: (state: State, commit: Commit) => Patch
+): Function {
+  return store => next => commit => {
+    const previousState = store.getState();
+    const result = next(commit);
+    const nextState = store.getState();
+    const now = new Date();
+    const type = typeof commit === 'object' && commit !== null ?
+      commit.type :
+      '';
+    console.group(`%c commit @ ${isoLocaleTimeString(now)} ${String(type)}`, style(colors.inherit));
+    console.log('%c prev state', style(colors.grey), previousState);
+    console.log('%c commit', style(colors.blue), commit);
+    console.log('%c patch', style(colors.blue), applyCommit(previousState, commit));
+    console.log('%c next state', style(colors.green), nextState);
+    console.groupEnd();
+    return result;
+  };
+}
+
 function snapshotShape<Effect, Commit, State>(
   snapshot: Snapshot<Effect, Commit, State>
 ): string[] {
   return snapshot.map((snapshotItem) => snapshotItem.type);
 }
 
-export default function* <Action, Effect, Commit, State, A>(
+export function* logShip<Action, Effect, Commit, State, A>(
   action: Action,
   ship: Ship<Effect, Commit, State, A>
 ): Ship<Effect, Commit, State, A> {
@@ -41,7 +63,7 @@ export default function* <Action, Effect, Commit, State, A>(
   const type = typeof action === 'object' && action !== null ?
     action.type :
     '';
-  console.group(`%c dispatch @ ${isoLocaleTimeString(now)} ${String(type)}`, style(colors.inherit));
+  console.group(`%c ship @ ${isoLocaleTimeString(now)} ${String(type)}`, style(colors.inherit));
   console.log('%c action', style(colors.blue), action);
   console.log('%c shape', style(colors.lightPurle), ...snapshotShape(snapshot));
   console.log('%c snapshot', style(colors.purle), snapshot);
